1c7b6530a511599fea2d82d9d181e111ff00559f
vijay <vijay-1122@users.noreply.github.com>
2016-12-29 11:59:59 +0530

    Add files via upload

---
+++ rec rev.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+
+    int rev[50];
+    printf("enter the no");
+    scanf("%d",&n);
+    while(n[++i]!='\0')
+        while(i>=0)
+        rev[j++]=n[--i];
+    rev[j]='\0';
+        printf("rev %d",rev);
+
+    return 0;
+
+}
+
+
