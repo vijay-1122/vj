8fa8a7385344d820de06d828172baf4b1a2825cd
vijay <vijay-1122@users.noreply.github.com>
2016-12-29 10:21:20 +0530

    Add files via upload

---
+++ ev.c.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the no");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("it is even");
+    }
+    else
+    {
+        printf("it is odd");
+    }
+    return 0;
+}
